ab2316e8a9391bf24d6fea36ac65dbb09c69a080
tpapka <36523428+tpapka@users.noreply.github.com>
2018-11-28 11:16:11 -0700

    Update bitcoin_price.py

--- bitcoin_price.py
+++ bitcoin_price.py
@@ -1,6 +1,17 @@
 import requests, datetime
 from bs4 import BeautifulSoup as bs
 
+#Yahoo Finance Version
+search = requests.get("https://finance.yahoo.com/quote/BTC-USD?p=BTC-USD")
+soup = bs(search.text, "html.parser")
+bitcoin_price = soup.find("span", class_ = "Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)")
+bp = bitcoin_price.get_text().split()
+t = datetime.datetime.now()
+t = str(t.time())[:-7] + " -"
+print(t, "$" + bp[0])
+
+
+# Google Version
 search = requests.get("https://www.google.com/search?q=bitcoin+price")
 soup = bs(search.text, "html.parser")
 bitcoin_price = soup.find(class_ = "J7UKTe")
