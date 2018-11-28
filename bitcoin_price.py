ecffcce2aead6c46e4f0b77e21fb971264c1918f
tpapka <36523428+tpapka@users.noreply.github.com>
2018-11-28 12:35:28 -0700

    Update bitcoin_price.py

--- bitcoin_price.py
+++ bitcoin_price.py
@@ -5,20 +5,26 @@ from bs4 import BeautifulSoup as bs
 search = requests.get("https://finance.yahoo.com/quote/BTC-USD?p=BTC-USD")
 soup = bs(search.text, "html.parser")
 bitcoin_price = soup.find("span", class_ = "Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)")
-bp = bitcoin_price.get_text().split()
-t = datetime.datetime.now()
-t = str(t.time())[:-7] + " -"
-print(t, "$" + bp[0])
+try:
+  bp = bitcoin_price.get_text().split()
+  t = datetime.datetime.now()
+  t = str(t.time())[:-7] + " -"
+  print(t, "$" + bp[0])
+except:
+  pass
 
 
 # Google Version
 search = requests.get("https://www.google.com/search?q=bitcoin+price")
 soup = bs(search.text, "html.parser")
 bitcoin_price = soup.find(class_ = "J7UKTe")
-bp = bitcoin_price.get_text().split()
-t = datetime.datetime.now()
-t = str(t.time())[:-7] + " -"
-print(t, "$" + bp[3])
+try:
+  bp = bitcoin_price.get_text().split()
+  t = datetime.datetime.now()
+  t = str(t.time())[:-7] + " -"
+  print(t, "$" + bp[3])
+except:
+  pass
 
 
 # Run it every three minutes as PowerShell function
